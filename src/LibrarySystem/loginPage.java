660544b3eb3c276e8a9a6aacaedc9afd3831dbe4
tyson medlin <tjmedlin@email.sc.edu>
2019-10-24 17:38:45 -0800

    Did some work on loginPage

--- src/LibrarySystem/loginPage.java
+++ src/LibrarySystem/loginPage.java
@@ -1,5 +1,41 @@
 package LibrarySystem;
+import java.util.Scanner;
 
 public class loginPage {
-
+	public loginPage()
+	{
+		int choice = 0;
+		startPage.getLine();
+		String[] choices = {"Login", "Create account","Return to home page"};
+		choice = startPage.getUserChoice(choices, choices.length);
+		getResponseForChoice(choice);
+	}
+	private void getResponseForChoice(int usersChoice)
+	{
+		Scanner key = new Scanner(System.in);
+		switch(usersChoice) {
+			case 1:
+				String name;
+				String password;
+				System.out.println("Please enter the account name");
+				name = key.nextLine();
+				System.out.println("Please enter the password");
+				password = key.nextLine();
+				login( password, name );
+				return;
+			case 2:
+				createNewUser();
+				return;
+		}
+		
+	}
+	public void createNewUser()
+	{
+		startPage.makeUserLookAtThisMessageLoop("This feature will be added once databases are ready");
+	}
+	public boolean login( String password, String name)
+	{
+		startPage.makeUserLookAtThisMessageLoop("This feature will be added once databases are ready");
+		return false;
+	}
 }
--- src/LibrarySystem/startPage.java
+++ src/LibrarySystem/startPage.java
@@ -1,3 +1,11 @@
+/*
+ * This class is the main class of the library system. 
+ * Finds out what the user wants to do, then calls that class.
+ * 
+ * Team: Untitled: Nick Bautista, Tyson Medlin, Luis Canales
+ * Main programmer of this class: Tyson Medlin
+ * Date: 10/24/2019
+ */
 package LibrarySystem;
 
 import java.text.DateFormat;
@@ -11,84 +19,115 @@ public class startPage {
 		
 	public static void main(String[] args) {
 		Scanner key = new Scanner(System.in);
-		
+		boolean exitProgram = false;
+		String[] choices = {"Login or create account","Logout","Browse available items","View fees","Pay fees","Return item","View holds","View wishlist"};
 		//get if user is signed in
 		
 		//The home screen
-		int usersChoice = getUserChoice();
-		getResponseForChoice(usersChoice);
-		
-	}
-	private static int getUserChoice()
-	{
-		int userChoice = 0;
-		int availableChoices = 1;
-		Scanner key = new Scanner(System.in);
-		while (userChoice > availableChoices || userChoice < 1){
+		while( exitProgram == false ) //infinite menu loop
+		{
 			System.out.println(getTitle());
 			System.out.println("Untitled Version 0.1 Date: " + getDate());
-			System.out.println(getLine());
-			String[] choices = {"Login or create account","Logout","Browse available items","View fees","Pay fees","Return item","View holds","View wishlist"};
-			System.out.println(choiceSetup(choices));
-			userChoice = key.nextInt();
-			availableChoices = choices.length;
+			if(isUserLoggedIn() == false) { System.out.println("Not currently logged in."); } else { System.out.println("Welcome, "); }
+			int usersChoice = getUserChoice(choices, choices.length);
+			getResponseForChoice(usersChoice);
 		}
-		return userChoice;
+		System.exit(0);
+	}
+	/* display a set of choices in a string array with matching numbers, with ease.
+	 * @param choices: the string array of all the choices you want given and  availableChoices: The amount of choices in that array just mentioned.
+	 * @returns the realistic user choice.
+	 */
+	public static int getUserChoice(String[] choices, int availableChoices)
+	{
+		String userChoice = "0";
+		Scanner key = new Scanner(System.in);
+		//Ensures we get an actual choice from the user and not anything else.
+		while (userChoice.matches("-?\\d+") == false || userChoice.matches("-?\\d+") == true && Integer.valueOf(userChoice) > availableChoices || userChoice.matches("-?\\d+") == true && Integer.valueOf(userChoice) < 1){
+			System.out.println(getLine());
+			System.out.println(choiceSetup(choices));
+			userChoice = key.nextLine();
+		}
+		return Integer.valueOf(userChoice);
 	}
 	private static void getResponseForChoice(int usersChoice)
 	{
 		switch(usersChoice) {
 			case 1:
 				login();
+				return;
 			case 2:
 				logout();
+				return;
 			case 3:
 				browse();
+				return;
 			case 4:
 				viewFee();
+				return;
 			case 5:
 				payFee();
+				return;
 			case 6:
 				returnItem();
+				return;
 			case 7:
 				viewHolds();
+				return;
 			case 8:
 				viewWishlist();
+				return;
 		}
 		
 	}
+	
+	//Main Choice Methods ----------------------------------------------------------------------------
 	private static void login()
 	{
-		System.out.println("test");
+		loginPage login = new loginPage();
 	}
 	private static void logout()
 	{
-		
+		if( isUserLoggedInLoop() == false) //If user is not logged in, make the acknowledge that, then return to home.
+		{ return; }
 	}
 	private static void browse()
 	{
-		
+		makeUserLookAtThisMessageLoop("This feature will be added once databases are ready"); //forces user to read the message before continuing.
 	}
 	private static void viewFee()
 	{
-		
+		if( isUserLoggedInLoop() == false) //If user is not logged in, make the acknowledge that, then return to home.
+		{ return; }
 	}
 	private static void returnItem()
 	{
-		
+		if( isUserLoggedInLoop() == false) //If user is not logged in, make the acknowledge that, then return to home.
+		{  return; }
 	}
 	private static void payFee()
 	{
-		
+		if( isUserLoggedInLoop() == false) //If user is not logged in, make the acknowledge that, then return to home.
+		{ return; }
 	}
+	private static void viewHolds()
+	{
+		if( isUserLoggedInLoop() == false) //If user is not logged in, make the acknowledge that, then return to home.
+		{ return; }
+	}
+	private static void viewWishlist()
+	{
+		if( isUserLoggedInLoop() == false) //If user is not logged in, make the acknowledge that, then return to home.
+		{ return; }
+	}
+	
+	
+	//Helper Methods ----------------------------------------------------------------------------
 	//public static user getCurrentUser()
 	//{
 		
 	//}
-	private static void viewHolds()
-	{
-		
-	}
+	
 	//Returns date in String in this format: yyyy/dd/MM HH:mm:ss
 	public static String getDate()
 	{
@@ -97,10 +136,6 @@ public class startPage {
 		Date Date = new Date();
 		date = (String)dateFormat.format(Date);
 		return date;
-	}
-	private static void viewWishlist()
-	{
-		
 	}
 	public static String getTitle()
 	{
@@ -120,5 +155,29 @@ public class startPage {
 			choiceDialogue =  choiceDialogue + (i+1) + ". " + choices[i] + "\n";
 		}
 		return choiceDialogue;
+	}
+	public static boolean isUserLoggedIn()
+	{
+		return false;
+	}
+	public static boolean isUserLoggedInLoop()
+	{
+		if( isUserLoggedIn() == false ) 
+		{
+			makeUserLookAtThisMessageLoop("Cannot perform this action without being logged in.");
+			
+		  return false;
+		}
+		return true;
+	}
+	public static void makeUserLookAtThisMessageLoop( String message )
+	{
+		Scanner key = new Scanner(System.in);
+		String choice = "2";
+		while(choice.equalsIgnoreCase("1") == false) {
+			System.out.println(message);
+			System.out.println("Return to main menu?. \n1. Yes\n2. No");
+		choice = key.nextLine();
+		}
 	}
 }
