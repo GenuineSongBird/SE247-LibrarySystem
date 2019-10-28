7cc523e5f43a043e91900d7bb82a97ff1c5ea0c1
Luis Canales <54644643+CanalesLuis@users.noreply.github.com>
2019-10-28 13:19:45 -0400

    Update checkoutLimitBehavior.java

--- src/LibrarySystem/checkoutLimitBehavior.java
+++ src/LibrarySystem/checkoutLimitBehavior.java
@@ -1,3 +1,10 @@
+/*
+ * This interface defines what the different checkoutLimits that the users can implement
+ *
+ * Team: Untitled: Nick Bautista, Tyson Medlin, Luis Canales
+ * Main programmer(s) of this class: Tyson Medlin, 
+ * Date: 10/24/2019
+ */
 package LibrarySystem;
 
 
