50b8b4f480a1722c56f1c6f05069cbaf1954a2c9
Luis Canales <54644643+CanalesLuis@users.noreply.github.com>
2019-10-28 13:32:02 -0400

    Update restrictedLimit.java

--- src/LibrarySystem/restrictedLimit.java
+++ src/LibrarySystem/restrictedLimit.java
@@ -1,3 +1,11 @@
+/*
+ * This class inherits from checkoutLimitBehavior and is called by user classes
+ * to define the classes checkoutLimit, extendedLimit is implemented by userChild
+ *
+ * Team: Untitled: Nick Bautista, Tyson Medlin, Luis Canales
+ * Main programmer(s) of this class: Luis Canales 
+ * Date: 10/28/2019
+ */
 package LibrarySystem;
 
 
