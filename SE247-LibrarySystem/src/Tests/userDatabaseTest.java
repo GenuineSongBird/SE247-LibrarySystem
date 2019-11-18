0f68154b895f188aa11cd5e2bba9c64749569e9a
Luis Canales <54644643+CanalesLuis@users.noreply.github.com>
2019-11-18 12:26:02 -0500

    Create userDatabaseTest.java

---
+++ SE247-LibrarySystem/src/Tests/userDatabaseTest.java
@@ -0,0 +1,20 @@
+package LibrarySystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org junit.Rule;
+
+import LibrarySystem.userChild;
+import LibrarySystem.userDatabase;
+import LibrarySystem.userPatron;
+import LibrarySystem.baseUser;
+
+public class itemDatabaseTest {
+
+	@Test
+	public void testLoad() {
+		userDatabase uDatabase = userDatabase.getInstance();
+		assertEquals(6, uDatabase.database.size());
+	}
+}
